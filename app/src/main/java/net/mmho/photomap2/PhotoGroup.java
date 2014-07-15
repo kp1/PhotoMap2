797cad692dadbb3853a8c65f3149d4570a25206a
kp <kp@mmho.net>
2014-07-15 13:12:59 +0900

    implement parcel in PhotoGroup.Group

--- app/src/main/java/net/mmho/photomap2/PhotoGroup.java
+++ app/src/main/java/net/mmho/photomap2/PhotoGroup.java
@@ -1,12 +1,14 @@
 package net.mmho.photomap2;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.LatLngBoundsCreator;
 import com.google.android.gms.maps.model.Marker;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
 public class PhotoGroup extends ArrayList<PhotoGroup.Group> {
@@ -16,16 +18,35 @@ public class PhotoGroup extends ArrayList<PhotoGroup.Group> {
         mCursor = c;
     }
 
-    public class Group{
+    public class Group implements Parcelable{
         public Marker marker;
         private LatLngBounds area;
-        private ArrayList<Long> list = new ArrayList<Long>();
+        private ArrayList<Long> id_list = new ArrayList<Long>();
+
+        public final Parcelable.Creator<Group> CREATOR = new Parcelable.Creator<Group>(){
+
+            @Override
+            public Group createFromParcel(Parcel in) {
+                final Group g = new Group(in);
+                return g;
+            }
+
+            @Override
+            public Group[] newArray(int size) {
+                return new Group[0];
+            }
+        };
+
+        Group(Parcel in){
+            in.readList(id_list, null);
+            area = LatLngBounds.CREATOR.createFromParcel(in);
+        }
 
         Group(LatLng p,long id){
             LatLngBounds.Builder b = new LatLngBounds.Builder();
             b.include(p);
             area = b.build();
-            list.add(id);
+            id_list.add(id);
         }
 
         public LatLng getCenter(){
@@ -41,22 +62,32 @@ public class PhotoGroup extends ArrayList<PhotoGroup.Group> {
         }
 
         public int size(){
-            return list.size();
+            return id_list.size();
         }
 
         public long getID(int index){
-            return list.get(index);
+            return id_list.get(index);
         }
 
         void append(LatLng point,long id){
             area = area.including(point);
-            list.add(id);
+            id_list.add(id);
         }
 
         public boolean equals(Group g) {
-            if(!list.equals(g.list)) return false;
-            if(!area.equals(g.area)) return false;
-            return true;
+            return id_list.equals(g.id_list) && area.equals(g.area);
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            out.writeList(id_list);
+            area.writeToParcel(out,0);
+
         }
     }
 
