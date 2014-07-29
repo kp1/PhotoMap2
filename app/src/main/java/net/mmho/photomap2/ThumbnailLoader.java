79f862a8d9925e7c9517e1990b774ccd2e9a2785
kp <kp@mmho.net>
2014-07-29 13:06:16 +0900

    Rotate thumbnail bitmap with orientation.

--- app/src/main/java/net/mmho/photomap2/ThumbnailLoader.java
+++ app/src/main/java/net/mmho/photomap2/ThumbnailLoader.java
@@ -1,7 +1,11 @@
 package net.mmho.photomap2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.content.AsyncTaskLoader;
 
@@ -9,17 +13,40 @@ public class ThumbnailLoader extends AsyncTaskLoader<Bitmap> {
 
     private long id;
     private Bitmap bitmap;
+    private Context context;
 
     public ThumbnailLoader(Context c,long id) {
         super(c);
         this.id = id;
+        context = c;
         onContentChanged();
     }
 
     @Override
     public Bitmap loadInBackground() {
-        return MediaStore.Images.Thumbnails.getThumbnail(getContext().getContentResolver(),id,
+        final String[] projection = {
+                MediaStore.Images.ImageColumns.ORIENTATION,
+        };
+        final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        Bitmap bmp = MediaStore.Images.Thumbnails.getThumbnail(getContext().getContentResolver(),id,
                 MediaStore.Images.Thumbnails.MINI_KIND,null);
+
+        Cursor c = MediaStore.Images.Media.query(context.getContentResolver(), uri, projection,
+                QueryBuilder.createQuery(id), null, null);
+
+        if(c.getColumnCount()>0){
+            c.moveToFirst();
+            int orientation = c.getInt(c.getColumnIndexOrThrow(MediaStore.Images.ImageColumns.ORIENTATION));
+            if(orientation!=0){
+                Matrix matrix = new Matrix();
+                matrix.setRotate(orientation);
+                bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+            }
+        }
+
+        c.close();
+        return bmp;
     }
 
     @Override
