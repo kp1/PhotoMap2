9408b30fe26e6048e380118e43bf440e8476d657
kp <kp@mmho.net>
2014-08-01 16:46:33 +0900

    set PhotoListFragment static.

--- app/src/main/java/net/mmho/photomap2/PhotoListActivity.java
+++ app/src/main/java/net/mmho/photomap2/PhotoListActivity.java
@@ -18,13 +18,5 @@ public class PhotoListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_photo_list);
 
         setProgress(0);
-
-        Fragment fragment = getSupportFragmentManager().findFragmentByTag(TAG_LIST);
-        if(fragment==null){
-            fragment = new PhotoListFragment();
-            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.add(android.R.id.content,fragment,TAG_LIST);
-            fragmentTransaction.commit();
-        }
     }
 }
