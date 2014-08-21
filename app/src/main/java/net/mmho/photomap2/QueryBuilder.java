5182ebd304cd31f5c161a9f4bf3a08a3845b6be9
kp <kp@mmho.net>
2014-08-21 15:32:17 +0900

    fix bug in map across 180 longitude.

--- app/src/main/java/net/mmho/photomap2/QueryBuilder.java
+++ app/src/main/java/net/mmho/photomap2/QueryBuilder.java
@@ -1,6 +1,7 @@
 package net.mmho.photomap2;
 
 import android.provider.MediaStore;
+import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
@@ -10,43 +11,44 @@ public class QueryBuilder {
     final static String LONGITUDE = MediaStore.Images.ImageColumns.LONGITUDE;
     final static String DATE_TAKEN = MediaStore.Images.ImageColumns.DATE_TAKEN;
     final static String IMAGE_ID = MediaStore.Images.ImageColumns._ID;
-    final static String BETWEEN = " BETWEEN ";
-    final static String AND = " AND ";
-    final static String OR = " OR ";
-    final static String IS = " IS ";
-    final static String NOT = " NOT ";
-    final static String NULL= " NULL ";
     static String createQuery(LatLngBounds bounds){
         LatLng start = bounds.southwest;
         LatLng end = bounds.northeast;
-        String q[] = {LATITUDE,BETWEEN, String.valueOf(start.latitude),AND, String.valueOf(end.latitude),
-                 AND, LONGITUDE,BETWEEN, String.valueOf(start.longitude),AND, String.valueOf(end.longitude)};
-        StringBuilder b = new StringBuilder();
-        for(String s:q){
-            b.append(s);
+        String latitude = String.format("%s between %f and %f and ",
+                LATITUDE,start.latitude,end.latitude);
+        String longitude;
+        if(start.longitude<end.longitude){
+            longitude= String.format("%s between %f and %f",
+                    LONGITUDE,start.longitude,end.longitude);
         }
-        return b.toString();
+        else{
+            longitude = String.format("(%s between -180.0 and %f or %s between %f and 180.0)",
+                    LONGITUDE,end.longitude,LONGITUDE,start.longitude);
+
+        }
+
+        return latitude+longitude;
 
     }
 
     static String createQuery(long id){
-        return IMAGE_ID+IS+id;
+        return IMAGE_ID+" is "+id;
     }
 
     static String createQuery(){
-        return LATITUDE+NOT+NULL+AND+LONGITUDE+NOT+NULL;
+        return LATITUDE+" not null and "+LONGITUDE+" not null";
     }
 
     static String createQueryNoLocation(){
-        return LATITUDE+IS+NULL+OR+LONGITUDE+IS+NULL;
+        return LATITUDE+" is null or "+LONGITUDE+"is null";
     }
 
     static String sortDateNewest(){
-        return DATE_TAKEN+" DESC";
+        return DATE_TAKEN+" desc";
     }
 
     static String sortDateOldest() {
-        return DATE_TAKEN+" ASC";
+        return DATE_TAKEN+" asc";
     }
 
 }
