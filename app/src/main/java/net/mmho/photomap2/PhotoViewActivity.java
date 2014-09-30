06446a97d5e4c860a54de1605d39fd62b01f7bc6
kp <kp@mmho.net>
2014-09-30 14:06:52 +0900

    写真の共有をActionProvider経由で行う

--- app/src/main/java/net/mmho/photomap2/PhotoViewActivity.java
+++ app/src/main/java/net/mmho/photomap2/PhotoViewActivity.java
@@ -5,10 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.WindowCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +23,7 @@ public class PhotoViewActivity extends ActionBarActivity {
     private PhotoViewAdapter adapter;
     private ViewPager pager;
     private long HIDE_DELAY=3*1000;
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +38,7 @@ public class PhotoViewActivity extends ActionBarActivity {
         supportRequestWindowFeature(WindowCompat.FEATURE_ACTION_BAR_OVERLAY);
         setContentView(R.layout.fragment_photo_view);
 
-        showActionBar(true);
+//        showActionBar(true);
         ActionBar bar = getSupportActionBar();
         if(bar!=null) bar.addOnMenuVisibilityListener(menuVisibilityListener);
 
@@ -48,6 +51,7 @@ public class PhotoViewActivity extends ActionBarActivity {
 
         pager = (ViewPager) findViewById(R.id.photo_pager);
         pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(onPageChangeListener);
         pager.setCurrentItem(position);
         pager.setPageMargin(30);
         pager.setOnClickListener(onClickListener);
@@ -97,40 +101,59 @@ public class PhotoViewActivity extends ActionBarActivity {
                     ActionBar bar = getSupportActionBar();
                     if(bar!=null){
                         if(bar.isShowing()) hideActionBar();
-                        else showActionBar(true);
+                        else showActionBar(false);
                     }
                 }
             };
 
+    final private ViewPager.OnPageChangeListener onPageChangeListener =
+            new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int i, float v, int i2) {
+
+                }
+
+                @Override
+                public void onPageSelected(int i) {
+                    if(shareActionProvider!=null)
+                        shareActionProvider.setShareIntent(setShareIntent(new Intent(),i));
+                }
+
+                @Override
+                public void onPageScrollStateChanged(int i) {
+
+                }
+            };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.photo_view_menu,menu);
-        return true;
+        MenuItem share = menu.findItem(R.id.share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(share);
+        shareActionProvider.setShareIntent(setShareIntent(new Intent(),pager.getCurrentItem()));
+
+        return super.onCreateOptionsMenu(menu);
     }
 
-    private void setUri(Intent intent){
-        final long image_id = adapter.getItemID(pager.getCurrentItem());
+    private Intent setShareIntent(Intent intent,int index){
+        final long image_id = adapter.getItemID(index);
         final Uri uri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 String.valueOf(image_id));
         intent.setAction(Intent.ACTION_SEND);
-        intent.setType("image/jpeg");
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         intent.putExtra(Intent.EXTRA_STREAM,uri);
+        intent.setType("image/jpeg");
+        return intent;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
         switch (item.getItemId()){
-        case R.id.share:
-            intent = new Intent();
-            setUri(intent);
-            startActivity(Intent.createChooser(intent,null));
-            return true;
         case R.id.map:
             intent = new Intent(this,PhotoMapActivity.class);
-            setUri(intent);
+            setShareIntent(intent,pager.getCurrentItem());
             startActivity(intent);
             return true;
         }
