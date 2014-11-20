add6eaac1009a300ad2b988686bcbc2416801516
kp <kp@mmho.net>
2014-11-20 15:26:19 +0900

    定数を参照する、未使用関数のwarningを抑制

--- app/src/main/java/net/mmho/photomap2/geohash/GeoHash.java
+++ app/src/main/java/net/mmho/photomap2/geohash/GeoHash.java
@@ -6,6 +6,7 @@ import android.os.Parcelable;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
+@SuppressWarnings("unused")
 public class GeoHash implements Parcelable {
 
     long bit;
