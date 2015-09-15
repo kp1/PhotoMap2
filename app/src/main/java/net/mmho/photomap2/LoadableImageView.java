47bd6b1a3d827aadcf2fbfda26969f81e92f5b69
kp <kp@mmho.net>
2015-09-15 14:21:33 +0900

    とりあえずサムネイルが読み込めている

--- app/src/main/java/net/mmho/photomap2/LoadableImageView.java
+++ app/src/main/java/net/mmho/photomap2/LoadableImageView.java
@@ -9,40 +9,59 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v4.util.LruCache;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 import rx.Observable;
 import rx.Observer;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
+import rx.subjects.BehaviorSubject;
 
 public class LoadableImageView extends ImageView{
 
+    private static final String TAG = "LoadableImageView";
     protected boolean thumbnail = false;
     private int width;
-    private long image_id = -1;
 
     public LoadableImageView(Context context) {
         super(context);
+        subscribeSubject();
     }
 
     public LoadableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        subscribeSubject();
     }
 
     public LoadableImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        subscribeSubject();
     }
 
+    BehaviorSubject<Long> subject;
+    Subscription subscription;
+    private void subscribeSubject(){
+        subject = BehaviorSubject.create();
+        subscription = subject
+            .onBackpressureDrop()
+            .doOnNext(image_id -> Log.d(TAG, "doOnNext:" + image_id))
+            .flatMap(this::loadImage)
+            .share()
+            .subscribe();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        subscription.unsubscribe();
+    }
 
     public void startLoading(final long image_id,final LruCache<Long,Bitmap> cache){
 
-        if(this.image_id==image_id){
-            return;
-        }
         Bitmap bmp = null;
-        this.image_id = image_id;
         if(cache!=null) bmp = cache.get(image_id);
         if(bmp!=null){
             setImageBitmap(bmp);
@@ -51,91 +70,86 @@ public class LoadableImageView extends ImageView{
 
         setImageDrawable(null);
 
-        post(new Runnable() {
-            @Override
-            public void run() {
-                width = Math.min(getWidth(), getHeight());
-                // load image
-                request()
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Observer<Bitmap>() {
-
-                        @Override
-                        public void onCompleted() {
-
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-
-                        }
-
-                        @Override
-                        public void onNext(Bitmap bitmap) {
-                            setImageBitmap(bitmap);
-                            if (cache != null) cache.put(image_id, bitmap);
-                        }
-                    });
-            }
+        post(() -> {
+            width = Math.min(getWidth(), getHeight());
+            subject.onNext(image_id);
         });
     }
 
-    private Observable<Bitmap> request(){
-        return Observable.create(new Observable.OnSubscribe<Bitmap>() {
-            @Override
-            public void call(Subscriber<? super Bitmap> subscriber) {
-                final String[] projection = {
-                    MediaStore.Images.ImageColumns._ID,
-                    MediaStore.Images.ImageColumns.ORIENTATION,
-                    MediaStore.Images.ImageColumns.DATA,
-                };
-                final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                Cursor c = MediaStore.Images.Media.query(getContext().getContentResolver(), uri, projection,
-                    QueryBuilder.createQuery(image_id), null, null);
+    private Observable<Bitmap> loadImage(Long image_id){
+        Observable<Bitmap> observable =
+        Observable.create( subscriber -> {
+            Log.d(TAG,"load image:"+image_id);
+            final String[] projection = {
+                MediaStore.Images.ImageColumns._ID,
+                MediaStore.Images.ImageColumns.ORIENTATION,
+                MediaStore.Images.ImageColumns.DATA,
+            };
+            final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            Cursor c = MediaStore.Images.Media.query(getContext().getContentResolver(), uri, projection,
+                QueryBuilder.createQuery(image_id), null, null);
 
-                Bitmap bmp = null;
+            Bitmap bmp = null;
 
-                if(thumbnail){
-                    bmp = MediaStore.Images.Thumbnails.getThumbnail(getContext().getContentResolver(),image_id,
-                        MediaStore.Images.Thumbnails.MINI_KIND,null);
-                }
-
-                if (c.getCount() > 0) {
-                    c.moveToFirst();
-                    int orientation = c.getInt(c.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION));
-
-                    if(!thumbnail) {
-                        BitmapFactory.Options option = new BitmapFactory.Options();
-                        String path = c.getString(c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
-
-                        // get only size
-                        option.inJustDecodeBounds = true;
-                        BitmapFactory.decodeFile(path, option);
-
-
-                        int s = Math.max(option.outHeight, option.outWidth) / width + 1;
-                        int scale = 1;
-                        while (scale < s) scale *= 2;
-
-                        option.inSampleSize = scale;
-                        option.inJustDecodeBounds = false;
-                        option.inPreferredConfig = Bitmap.Config.RGB_565;
-                        bmp = BitmapFactory.decodeFile(path, option);
-                    }
-
-                    if (bmp!=null && orientation != 0) {
-                        Matrix matrix = new Matrix();
-                        matrix.setRotate(orientation);
-                        Bitmap oldBmp = bmp;
-                        bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
-                        oldBmp.recycle();
-                    }
-                }
-                c.close();
-                subscriber.onNext(bmp);
-                subscriber.onCompleted();
+            if(thumbnail){
+                bmp = MediaStore.Images.Thumbnails.getThumbnail(getContext().getContentResolver(),image_id,
+                    MediaStore.Images.Thumbnails.MINI_KIND,null);
             }
+
+            if (c.getCount() > 0) {
+                c.moveToFirst();
+                int orientation = c.getInt(c.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION));
+
+                if(!thumbnail) {
+                    BitmapFactory.Options option = new BitmapFactory.Options();
+                    String path = c.getString(c.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+
+                    // get only size
+                    option.inJustDecodeBounds = true;
+                    BitmapFactory.decodeFile(path, option);
+
+
+                    int s = Math.max(option.outHeight, option.outWidth) / width + 1;
+                    int scale = 1;
+                    while (scale < s) scale *= 2;
+
+                    option.inSampleSize = scale;
+                    option.inJustDecodeBounds = false;
+                    option.inPreferredConfig = Bitmap.Config.RGB_565;
+                    bmp = BitmapFactory.decodeFile(path, option);
+                }
+
+                if (bmp!=null && orientation != 0) {
+                    Matrix matrix = new Matrix();
+                    matrix.setRotate(orientation);
+                    Bitmap oldBmp = bmp;
+                    bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), matrix, false);
+                    oldBmp.recycle();
+                }
+            }
+            subscriber.onNext(bmp);
+            subscriber.onCompleted();
+            c.close();
         });
+        observable
+            .observeOn(Schedulers.newThread())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Observer<Bitmap>() {
+                @Override
+                public void onCompleted() {
+
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onNext(Bitmap bitmap) {
+                    setImageBitmap(bitmap);
+                }
+            });
+        return observable;
     }
 }
