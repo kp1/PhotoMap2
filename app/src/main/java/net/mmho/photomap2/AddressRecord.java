44516bab64010c5003af91c7e1a9470dbee1fd6a
kp <kp@mmho.net>
2015-11-10 15:31:03 +0900

    住所情報の保持をOllieで行う

--- app/src/main/java/net/mmho/photomap2/AddressRecord.java
+++ app/src/main/java/net/mmho/photomap2/AddressRecord.java
@@ -1,25 +1,24 @@
 package net.mmho.photomap2;
 
-import com.activeandroid.Model;
-import com.activeandroid.annotation.Column;
-import com.activeandroid.annotation.Table;
-import com.activeandroid.query.Delete;
-import com.activeandroid.query.Select;
-
 import net.mmho.photomap2.geohash.GeoHash;
 
-@Table(name="AddressTable")
-public class AddressRecord extends Model{
-    @Column(name="address")
-    private String address;
-    @Column(name="description")
-    private String description;
-    @Column(name="hash")
-    private long hash;
-    @Column(name="length")
-    private int length;
+import ollie.Model;
+import ollie.annotation.Column;
+import ollie.annotation.Table;
+import ollie.query.Delete;
+import ollie.query.Select;
 
-    @SuppressWarnings("unused")
+@Table("addresses")
+public class AddressRecord extends Model {
+    @Column("address")
+    public String address;
+    @Column("description")
+    public String description;
+    @Column("hash")
+    public Long hash;
+    @Column("length")
+    public Integer length;
+
     public AddressRecord(){
         super();
     }
@@ -41,12 +40,12 @@ public class AddressRecord extends Model{
     }
 
     public static AddressRecord getAddressByHash(GeoHash hash){
-        return new Select().from(AddressRecord.class)
-                .where("hash = ?", hash.getLong()).and("length = ?",hash.getSignificantBits())
-                .executeSingle();
+        return Select.from(AddressRecord.class)
+                .where("hash = ? and length = ?", hash.getLong(),hash.getSignificantBits())
+                .fetchSingle();
     }
 
     public static void clearCache(){
-        new Delete().from(AddressRecord.class).execute();
+        Delete.from(AddressRecord.class).execute();
     }
 }
---
+++ app/src/main/java/net/mmho/photomap2/PhotoMapApplication.java
@@ -0,0 +1,21 @@
+package net.mmho.photomap2;
+
+import android.app.Application;
+
+import ollie.Ollie;
+
+public class PhotoMapApplication extends Application {
+
+    private final static String DB_NAME="photomap.db";
+    private final static int DB_VERSION = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Ollie.with(this)
+            .setName(DB_NAME)
+            .setVersion(DB_VERSION)
+            .setLogLevel(Ollie.LogLevel.FULL)
+            .init();
+    }
+}
