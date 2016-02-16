be3fac1b4c322e0a9df98a4cf1c4437ff7fc28e1
kp <kp@mmho.net>
2016-02-16 15:20:03 +0900

    一枚表示時のシングルタップのコードを修正

--- app/src/main/java/net/mmho/photomap2/PhotoViewPager.java
+++ app/src/main/java/net/mmho/photomap2/PhotoViewPager.java
@@ -1,53 +1,34 @@
 package net.mmho.photomap2;
 
 import android.content.Context;
-import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 
 public class PhotoViewPager extends ViewPager{
 
+    private GestureDetectorCompat detector;
+
     public PhotoViewPager(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public PhotoViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        detector = new GestureDetectorCompat(context,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                performClick();
+                return true;
+            }
+        });
     }
-
-
-    private float mLastTouchX,mLastTouchY;
-    private final int slop = ViewConfiguration.get(getContext()).getScaledEdgeSlop();
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
-
-        final int action = MotionEventCompat.getActionMasked(ev);
-
-
-        switch(action){
-            case MotionEvent.ACTION_DOWN: {
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-                mLastTouchX = x;
-                mLastTouchY = y;
-                break;
-            }
-            case MotionEvent.ACTION_UP: {
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-
-                if(Math.abs(mLastTouchX-x)<slop && Math.abs(mLastTouchY-y)<slop){
-                    performClick();
-                }
-                break;
-            }
-
-        }
+        detector.onTouchEvent(ev);
         return super.onTouchEvent(ev);
     }
 
