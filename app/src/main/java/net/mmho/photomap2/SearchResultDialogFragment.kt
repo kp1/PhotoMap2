7fdb49cb4dae6a0816424a7d858e2667530ec13d
kp <kp@mmho.net>
2016-04-13 11:06:52 +0900

    SearchResultDialogFragmentをKotlin化

---
+++ app/src/main/java/net/mmho/photomap2/SearchResultDialogFragment.kt
@@ -0,0 +1,42 @@
+package net.mmho.photomap2
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.location.Address
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+
+class SearchResultDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val title = arguments.getString("title")
+        val list = arguments.getParcelableArray("address") as Array<Address>
+        val adapter = AddressListAdapter(activity, android.R.layout.simple_list_item_2, list)
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle(getString(R.string.search_title, title))
+        builder.setAdapter(adapter) { dialog, which ->
+            val f = this@SearchResultDialogFragment.activity.supportFragmentManager.findFragmentById(R.id.map)
+            if (f is PhotoMapFragment) {
+                val update = CameraUpdateFactory.newLatLngZoom(AddressUtil.addressToLatLng(list[which]),
+                    PhotoMapFragment.DEFAULT_ZOOM)
+                f.getMapAsync { map -> map.moveCamera(update) }
+            }
+        }
+        return builder.create()
+
+    }
+
+    companion object {
+        internal fun newInstance(location: String, addresses: List<Address>): SearchResultDialogFragment {
+            val fragment = SearchResultDialogFragment()
+
+
+            val bundle = Bundle()
+            bundle.putString("title", location)
+            bundle.putParcelableArray("address", addresses.toTypedArray())
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
