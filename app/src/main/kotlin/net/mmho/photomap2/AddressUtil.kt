c4bf0f9a8f546ba50d6aedd715c410a129900731
kp <kp@mmho.net>
2016-04-14 13:17:27 +0900

    住所の詳細情報のコードをシンプルにした

--- app/src/main/kotlin/net/mmho/photomap2/AddressUtil.kt
+++ app/src/main/kotlin/net/mmho/photomap2/AddressUtil.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.location.Address
 import com.google.android.gms.maps.model.LatLng
 
-private fun removePostalCode(source: String): String {
-    return source.replaceFirst("〒[0-9¥-]*".toRegex(), "")
-}
-
 fun Address.getTitle(c:Context):String{
     return buildString {
         val sep = c.getString(R.string.address_separator)
@@ -20,21 +16,13 @@ fun Address.getTitle(c:Context):String{
 
 fun Address.getDescription() : String {
 
-    return removePostalCode(buildString {
-        when (this@getDescription.maxAddressLineIndex) {
-            0 -> {
-                append(this@getDescription.getAddressLine(0))
-            }
-            else -> {
-                var i = 1
-                do {
-                    val line = this@getDescription.getAddressLine(i)
-                    append(line)
-                    i++
-                } while (line != null)
-            }
-        }
-    });
+    return buildString {
+        var i = 0
+        do{
+            val line = this@getDescription.getAddressLine(i++)
+            append(line?:""," ")
+        }while(line!=null)
+    }
 }
 
 fun Address.toLatLng():LatLng{
