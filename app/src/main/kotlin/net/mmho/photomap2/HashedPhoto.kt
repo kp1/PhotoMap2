25e6838eb3006b8787f21ae81c4d6769da7e8a9c
kp <kp@mmho.net>
2016-09-07 10:56:24 +0900

    Parcelableの実装を修正

--- app/src/main/kotlin/net/mmho/photomap2/HashedPhoto.kt
+++ app/src/main/kotlin/net/mmho/photomap2/HashedPhoto.kt
@@ -6,31 +6,19 @@ import android.os.Parcelable
 import net.mmho.photomap2.geohash.GeoHash
 
 data class HashedPhoto(val photo_id:Long, val hash:GeoHash) : Parcelable {
+    constructor(source: Parcel): this(source.readLong(), source.readParcelable<GeoHash>(GeoHash::class.java.classLoader))
 
-    override fun describeContents(): Int {
-        return 0
-    }
+    override fun describeContents() = 0
 
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeLong(this.photo_id)
-        hash.writeToParcel(dest, flags)
-    }
-
-    private constructor(input: Parcel):
-        this(input.readLong(),GeoHash.CREATOR.createFromParcel(input)) {
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeLong(photo_id)
+        dest?.writeParcelable(hash, 0)
     }
 
     companion object {
-
-        val CREATOR: Parcelable.Creator<HashedPhoto> = object : Parcelable.Creator<HashedPhoto> {
-            override fun createFromParcel(source: Parcel): HashedPhoto {
-                return HashedPhoto(source)
-            }
-
-            override fun newArray(size: Int): Array<HashedPhoto?> {
-                return arrayOfNulls(size)
-            }
+        @JvmField val CREATOR: Parcelable.Creator<HashedPhoto> = object : Parcelable.Creator<HashedPhoto> {
+            override fun createFromParcel(source: Parcel): HashedPhoto = HashedPhoto(source)
+            override fun newArray(size: Int): Array<HashedPhoto?> = arrayOfNulls(size)
         }
     }
-
 }
